2f3b1bd420af8493c92b3f5491d8bba899d80de6
Fabricio_Guima <fsgkof@gmail.com>
2025-08-21 22:27:30 -0300

    tailwindcss class

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -1,6 +1,14 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
-<body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Form</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="flex items-center justify-center min-h-screen bg-gray-100">
     <livewire:company-create />
 </body>
 
